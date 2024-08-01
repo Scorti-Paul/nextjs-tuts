85659ad93738c079ca7c5d8e7f18fc93cbfebdb5
Scorti-Paul <scortip@gmail.com>
2024-08-01 16:41:25 +0100

    initial commit

---
+++ src/app/(pages)/(auth)/layout.tsx
@@ -0,0 +1,14 @@
+const AuthLayout = ({
+  children
+}: {
+  children: React.ReactNode
+}) => {
+  return (
+    <div>
+      <h3>Auth Layout</h3>
+      {children}
+    </div>
+  )
+}
+
+export default AuthLayout
---
+++ src/app/(pages)/welcome/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = ({
+  title: "Welcome",
+  Description: "welcome page for the website"
+})
+const Welcome = () => {
+  return (
+    <>
+      <h2>Welcome Page</h2>
+    </>
+  )
+}
+
+export default Welcome
